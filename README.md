fe8448e3b8e685f10e11d0780b643b670b42f713
Amr Faisal <meamary85@gmail.com>
2020-02-24 11:29:06 +0200

    Adding CLI comannds

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-chain
-Simple Blockchain implementation with Go
+Simple Blockchain implementation in Go
+
+`go-chain addblock -data "My New Block"`
+
+`go-chain printchain`
--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockchain()
 	defer bc.db.Close()
 
-	bc.AddBlock("Data 1")
-	bc.AddBlock("Data 2")
-	bc.AddBlock("data 3")
-
-	bci := bc.Iterator()
-
-	for {
-		block := bci.Next()
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
